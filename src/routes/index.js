5babadf76ccac209585d08bc6da7abc39c0e63c7
yosassenav <mromerovanessa.soy@gmail.com>
2022-12-03 00:01:23 -0500

    adding changes to users endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
+const { json } = require("express");
 const usersRouter = require("./users");
 const todosRouter = require("./todos");
 const categoriesRouter = require("./categories");
 
 const apiRouter = (app) => {
+  app.use(json());
   app.use("/users", usersRouter);
   //app.use("/categories", categoriesRouter);
   //app.use("/todos", todosRouter);
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,16 +1,20 @@
 const { Router } = require("express");
+const fs = require("fs/promises");
 //const express = require("express");
 
 const router = Router();
 //express.Router();
 
 //GET All Users
-router.get("/", (req, res) => {
-  users = [
-    { id: 1, username: "jex", password: "123" },
-    { id: 2, username: "vanne", password: "342" },
-    { id: 3, username: "diego", password: "062" },
-  ];
+router.get("/", async (req, res) => {
+  const fileContent = await (await fs.readFile("./usersdummy.json")).toString();
+  const users = JSON.parse(fileContent);
+
+  // users = [
+  //   { id: 1, username: "jex", password: "123" },
+  //   { id: 2, username: "vanne", password: "342" },
+  //   { id: 3, username: "diego", password: "062" },
+  // ];
   res.json(users);
 });
 
@@ -28,13 +32,20 @@ router.get("/:userid", (req, res) => {
 });
 
 //Post One User (create)
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const data = req.body;
-  const { username, email } = data;
-  const newUser = { username, email, id: 54 };
+
+  const { username, email, id } = data;
+  const newUser = { username, email, id };
+
+  const fileContent = await (await fs.readFile("./usersdummy.json")).toString();
+  const users = JSON.parse(fileContent);
+  users.push(newUser);
+
+  await fs.writeFile("./usersdummy.json", JSON.stringify(users));
 
   if (!data) {
-    res.status(400).json({ message: "User data is required" });
+    res.status(404).json({ message: "User data is required" });
   } else {
     res.status(201).json({
       ok: true,
@@ -45,6 +56,33 @@ router.post("/", (req, res) => {
 });
 
 //Put One User (update)
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { username } = req.body;
+
+  console.log(id);
+
+  const fileContent = (await fs.readFile("./usersdummy.json")).toString();
+  const users = JSON.parse(fileContent);
+  console.log("users: ", users);
+
+  const user = users.find((item) => {
+    return item.id == id;
+  });
+
+  const newUsers = users.filter((item) => {
+    return id != item.id;
+  });
+  console.log("user: ", user);
+  user.username = username;
+
+  newUsers.push(user);
+
+  console.log(newUsers);
+  await fs.writeFile("./usersdummy.json", JSON.stringify(newUsers));
+
+  res.json({ ok: true, payload: user });
+});
 
 //Delete One User
 
